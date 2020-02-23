f7cd4afe1b3c3b5d019f4c98977aaa614803e94b
Akansha0211 <akansha.saxena_cs18@gla.ac.in>
2020-02-23 13:47:46 +0530

    create object of ContactListApplication

--- src/execution/ContactListAppMain.java
+++ src/execution/ContactListAppMain.java
@@ -1,10 +1,12 @@
 package execution;
 
 
+import ContactListApplication.ContactListApplication;
+
 import java.util.Scanner;
 
 public class ContactListAppMain {
     private static Scanner scanner = new Scanner(System.in);
-
+    private static ContactListApplication ca = new ContactListApplication();
 
 }
