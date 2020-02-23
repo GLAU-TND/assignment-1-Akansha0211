91cf7692bee525ca1a7a5835551e93215400b7af
Akansha0211 <akansha.saxena_cs18@gla.ac.in>
2020-02-23 14:02:10 +0530

    create switch statement and its cases and accprdingly create methods

--- src/execution/ContactListAppMain.java
+++ src/execution/ContactListAppMain.java
@@ -9,4 +9,51 @@ public class ContactListAppMain {
     private static Scanner scanner = new Scanner(System.in);
     private static ContactListApplication ca = new ContactListApplication();
 
+    public static void main(String[] args) {
+        boolean quit = false;
+        int choice = 0;
+        while (!quit) {
+            System.out.println("Enter your choice");
+            choice = scanner.nextInt();
+            scanner.nextLine();
+            switch (choice) {
+                case 0:
+                    printInstructions();
+                    break;
+                case 1:
+                    addContact();
+                    break;
+                case 2:
+                    ca.print();
+                    break;
+                case 3:
+                    searchContact();
+                    break;
+                case 4:
+                    removeContact();
+                    break;
+                case 5:
+                    quit = true;
+                    break;
+            }
+        }
+    }
+
+    public static void printInstructions() {
+
+    }
+
+    public static void addContact() {
+
+    }
+
+    public static void searchContact() {
+
+    }
+
+    public static void removeContact() {
+
+    }
+
+
 }
