040653a8673d893a49dfaf1da0f3ae6f10a452b9
Akansha0211 <akansha.saxena_cs18@gla.ac.in>
2020-02-23 10:51:13 +0530

    create ArrayList of Person class by creating its object

---
+++ src/ContactListApplication/ContactListApplication.java
@@ -0,0 +1,9 @@
+package ContactListApplication;
+
+import details.Person;
+
+import java.util.ArrayList;
+
+public class ContactListApplication {
+    private ArrayList<Person> contactList = new ArrayList<>();
+}
